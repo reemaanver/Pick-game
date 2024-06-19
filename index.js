8bfc3d4e0f2bda50407dcca344c3ab681eb36e27
reemaanver <108041797+reemaanver@users.noreply.github.com>
2024-06-19 22:19:39 +0530

    linked multi.html page

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-"use strict";
+'use strict';
+
+const rulesBtn = document.querySelector('.btn-rules');
+const rulesModal = document.querySelector('.game-rules');
+const overlay = document.querySelector('.overlay');
+const rulesCloseBtn = document.querySelector('.rules-close');
+const multiBtn = document.querySelector('.btn-multi');
 
 // GAME RULES MODAL
 
-const rulesBtn = document.querySelector(".btn-rules");
-const rulesModal = document.querySelector(".game-rules");
-const overlay = document.querySelector(".overlay");
-const rulesCloseBtn = document.querySelector(".rules-close");
-
 const openRules = function () {
-  rulesModal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  rulesModal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 const closeRules = function () {
-  rulesModal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  rulesModal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-rulesBtn.addEventListener("click", openRules);
+rulesBtn.addEventListener('click', openRules);
 
-rulesCloseBtn.addEventListener("click", closeRules);
+rulesCloseBtn.addEventListener('click', closeRules);
 
-overlay.addEventListener("click", closeRules);
+overlay.addEventListener('click', closeRules);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    if (!rulesModal.classList.contains("hidden")) {
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!rulesModal.classList.contains('hidden')) {
       closeRules();
     }
   }
+});
+
+// 2 PLAYER BUTTTON
+
+multiBtn.addEventListener('click', function () {
+  window.location.href = 'multi.html';
 });
